f1746026f645238859445715347015aaa9b152e0
angelolavorato <angelolavorato@hotmail.com>
2026-02-09 23:24:01 -0300

    iniciando conteúdos sobre variáveis

---
+++ FullStack_Rocketseat/JavaScript/Variaveis/scripts.js
@@ -0,0 +1,11 @@
+//Declarando uma variável sem valor
+var user
+console.log(user) //undefined
+
+//Declarando uma variável com valor
+var email = 'angelo@email.com'
+console.log(email) //
+
+//substituindo o valor da variável
+email = 'pedro@email.com.br'
+console.log(email)
