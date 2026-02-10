40aae2dad948bcbbf0f91e0a0491ec36033368cd
angelolavorato <angelolavorato@hotmail.com>
2026-02-09 23:09:02 -0300

    A

---
+++ FullStack_Rocketseat/JavaScript/Aula2/scripts.js
@@ -0,0 +1,7 @@
+//Sintaxe correta
+console.log('Olá, mundo!')
+//Sintaxe incorreta
+log.console('Olá, mundo!')
+//Utilzação de ; é opcional, mas é recomendado para evitar erros de interpretação do código. exemplo:
+console.log('Olá, mundo!')
+console.log('Bem-vindo ao JavaScript')
